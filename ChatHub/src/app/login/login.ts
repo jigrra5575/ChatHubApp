d00c28c4c110950bb12346c7b67e5a98642896de
JIGRRA <jigarchovatiya12345@gmail.com>
2026-01-19 10:39:52 +0530

    Add files via upload

--- ChatHub/src/app/login/login.ts
+++ ChatHub/src/app/login/login.ts
@@ -1,1 +1,56 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Service } from '../Services/service';
+import { AuthenticationService } from '../Services/authentication-service';
 
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+  constructor(
+    private service: Service,
+    private authentication: AuthenticationService,
+    private route: Router
+  ) { }
+
+  message = null;
+  err: any;
+
+  onsubmit(email: any, password: any) {
+
+    localStorage.setItem('password', password);
+
+    this.authentication.login(email, password).subscribe({
+      next: (res: any) => {
+        if (res) {
+          localStorage.setItem('logged', 'true');
+          localStorage.setItem('token', res.token);
+          localStorage.setItem('nick', res.username);
+          localStorage.setItem('img', res.img);
+
+          //* protected data get
+          this.service.getprotecteddata().subscribe((res: any) => {
+            this.message = res;
+            if (this.message != null) {
+              this.route.navigate(['/chat']);
+              setTimeout(() => {
+                this.message = null;
+              }, 3000);
+            } else {
+              console.log('Unauthorized access');
+            }
+          });
+        } else {
+          this.err.push('Unauthorized access');
+        }
+      },
+      error: () => {
+        this.err.push('Unauthorized access');
+      }
+    });
+  }
+}
